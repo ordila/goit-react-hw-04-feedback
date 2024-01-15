fada42ba80f4ac986c70148a764e801ec81a4827
ordila <ordila99@gmail.com>
2024-01-15 16:11:33 +0200

    remove class components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,76 +1,66 @@
 import { Button } from './Button/Button';
 import { Statistic } from './Statistic/Statistic';
-
+import React, { FC, useState } from 'react';
 import css from './App.module.css';
-import { Component } from 'react';
 import { Section } from './Section/Section';
 import { Notifications } from './Notification/Notifications';
 
-interface IProps {
+interface IState {
   good: number;
   neutral: number;
   bad: number;
 }
 
-export class App extends Component<{}, IProps> {
-  state = {
+export const App: FC = () => {
+  const [statistic, setStatistic] = useState<IState>({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  handleButton = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const { good, neutral, bad } = statistic;
+
+  const handleButton = (event: React.MouseEvent<HTMLButtonElement>) => {
     const name = (event.target as HTMLButtonElement).name;
-    this.setState(
-      prevState =>
-        ({
-          ...prevState,
-          [name]: prevState[name as keyof IProps] + 1,
-        } as IProps)
-    );
+    setStatistic(prevState => ({
+      ...prevState,
+      [name]: prevState[name as keyof IState] + 1,
+    }));
   };
 
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  }
-  countPositiveFeedbackPercentage() {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
-    return total > 0 ? (good / total) * 100 : 0;
-  }
+  const getStatistics = () => {
+    const total = good + neutral + bad;
+    const percantage = total > 0 ? (good / total) * 100 : 0;
+    return {
+      good,
+      neutral,
+      bad,
+      total,
+      percantage: Math.round(percantage),
+    };
+  };
+  const statistics = getStatistics();
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const percantage = this.countPositiveFeedbackPercentage();
-    const options = Object.keys(this.state);
+  const options = Object.keys(statistic);
 
-    return (
-      <div className={css.container}>
-        <Section title="Please leave your feedback">
-          <div className={css.buttons_wrapper}>
-            {options.map(el => (
-              <Button key={el} name={el} handleClick={this.handleButton}>
-                {el}
-              </Button>
-            ))}
-          </div>
+  return (
+    <div className={css.container}>
+      <Section title="Please leave your feedback">
+        <div className={css.buttons_wrapper}>
+          {options.map(el => (
+            <Button key={el} name={el} handleClick={handleButton}>
+              {el}
+            </Button>
+          ))}
+        </div>
+      </Section>
+      {statistics.total > 0 ? (
+        <Section title="Statistics">
+          <Statistic {...statistics} />
         </Section>
-        {total > 0 ? (
-          <Section title="Statistics">
-            <Statistic
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              percantage={Math.round(percantage)}
-            />
-          </Section>
-        ) : (
-          <Notifications message="There is no feedback" />
-        )}
-      </div>
-    );
-  }
-}
+      ) : (
+        <Notifications message="There is no feedback" />
+      )}
+    </div>
+  );
+};
--- src/components/Statistic/Statistic.tsx
+++ src/components/Statistic/Statistic.tsx
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import { FC } from 'react';
 import { IStatisticProps } from './Statistic.types';
 
-export class Statistic extends Component<IStatisticProps> {
-  render() {
-    const { good, neutral, bad, total, percantage } = this.props;
-
-    return (
-      <div>
-        <p>Good : {good}</p>
-        <p>Neutral : {neutral}</p>
-        <p>Bad : {bad}</p>
-        <p>Total : {total}</p>
-        <p>Percantage: {percantage}%</p>
-      </div>
-    );
-  }
-}
+export const Statistic: FC<IStatisticProps> = ({
+  good,
+  neutral,
+  bad,
+  total,
+  percantage,
+}) => {
+  return (
+    <div>
+      <p>Good : {good}</p>
+      <p>Neutral : {neutral}</p>
+      <p>Bad : {bad}</p>
+      <p>Total : {total}</p>
+      <p>Percantage: {percantage}%</p>
+    </div>
+  );
+};
